01d8b8a094700e203689a6d6de3ff6fc947417c9
axel.gr <axel.guillouard@icloud.com>
2023-10-09 11:29:18 +0200

    modif app

--- filine-app/app/more/page.tsx
+++ filine-app/app/more/page.tsx
@@ -1,20 +1,27 @@
 "use client"
-import { subtitle, title } from "@/components/primitives";
-import { Input } from "@nextui-org/input";
-import { Button } from "@nextui-org/button";
-import { Divider } from "@nextui-org/divider";
-import { Card, CardBody } from "@nextui-org/card";
+import {subtitle, title} from "@/components/primitives";
+import {Input} from "@nextui-org/input";
+import {Button} from "@nextui-org/button";
+import {Divider} from "@nextui-org/divider";
+import {Card, CardBody} from "@nextui-org/card";
 import {Avatar} from "@nextui-org/react";
 import {ThemeSwitch} from "@/components/theme-switch";
+import React from "react";
+import {Arrow} from "@/components/icons";
+import {Link} from "@nextui-org/link";
 
 export default function VirementPage() {
     return (
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-lg">
             <h1 className={`${title()} text-left md:text-left`}>Plus</h1>
-            <Card>
+            <div className="mt-2 flex justify-center items-center">
+            <Card className="w-[350px]">
                 <CardBody>
                     <div className="flex place-content-around">
-                        <Avatar name="AG" />
+                        <Avatar isBordered color="warning" classNames={{
+                            base: "bg-gradient-to-br from-[#FFB457] to-[#FF705B]"
+                        }}
+                                src="/memoji_ag.png" size="lg" />
                         <div className="mx-5 grid">
                             <h2 className="text-lg font-semibold">Axel Guillouard</h2>
                             <p className="text-xs">Profile et données personnel</p>
@@ -22,17 +29,54 @@ export default function VirementPage() {
                     </div>
                 </CardBody>
             </Card>
-
-            <Divider className="mt-3" />
-            <div className="flex justi">
-            <p className="text-left font-semibold">Mutuelle</p>
             </div>
-            <Divider className="mt-0" />
-            <p className="text-left font-semibold">Souscrire</p>
-            <Divider className="mt-0" />
-            <p className="text-left font-semibold">Contactez mon agence</p>
-            <Divider className="mt-0" />
-            <ThemeSwitch/>
+
+            <Divider className="mt-3"/>
+            <Link
+                href="/mutuelle"
+                className="flex justify-between mt-2 mb-2 text-current"
+            >
+                <p className="text-left font-semibold">Mutuelle</p>
+                <Arrow></Arrow>
+            </Link>
+
+            <Divider className="mt-0"/>
+
+            <Link
+                href="/mutuelle"
+                className="flex justify-between mt-2 mb-2 text-current"
+            >
+                <p className="text-left font-semibold">Souscrire</p>
+                <Arrow></Arrow>
+            </Link>
+
+            <Divider className="mt-0"/>
+
+            <Link
+                href="/contact"
+                className="flex justify-between mt-2 mb-2 text-current"
+            >
+                <p className="text-left font-semibold">Contactez mon agence</p>
+                <Arrow></Arrow>
+            </Link>
+
+            <Divider className="mt-0"/>
+
+            <Link
+                href="/credit"
+                className="flex justify-between mt-2 mb-2 text-current"
+            >
+                <p className="text-left font-semibold">Demande de crédit conso</p>
+                <Arrow></Arrow>
+            </Link>
+
+            <Divider className="mt-0"/>
+
+            <div className="flex justify-between mt-2 mb-2 text-current">
+                <p>Changer le thème de l'apps</p>
+                <ThemeSwitch/>
+            </div>
+
         </div>
     );
 }
--- filine-app/components/pretconso.tsx
+++ filine-app/components/pretconso.tsx
@@ -9,25 +9,11 @@ export default function AssuVie() {
         <>
             <div className="grid w-full text-left items-center">
                 <h2>Pret consomation</h2>
-                <Card className="mt-4">
-                    <CardBody>
-                        <div className="flex justify-between">
-                            <div className="px-2 text-left">
-                                <p><strong>Assurance mixte</strong></p>
-                                <p>M. PRENOM NOM</p>
-                            </div>
-                            <div className="px-2 text-center">
-                                <p className="text-green-600 font-bold">+ 200 000€</p>
-                                <Link>
-                                    <Button size="sm" radius="full" color="primary">
-                                        Consulter
-                                    </Button>
-                                </Link>
-                            </div>
-                        </div>
 
-                    </CardBody>
-                </Card>
+                <div>
+
+                </div>
+
 
             </div>
         </>
