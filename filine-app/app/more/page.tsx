d284057c21e554dae965385c48593da65825b14f
axel.gr <axel.guillouard@icloud.com>
2023-10-12 10:48:15 +0200

    modification more et carte

--- filine-app/app/more/page.tsx
+++ filine-app/app/more/page.tsx
@@ -6,11 +6,37 @@ import {Divider} from "@nextui-org/divider";
 import {Card, CardBody} from "@nextui-org/card";
 import {Avatar} from "@nextui-org/react";
 import {ThemeSwitch} from "@/components/theme-switch";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Arrow} from "@/components/icons";
 import {Link} from "@nextui-org/link";
+import {useRouter} from "next/navigation";
+import {fetchUserData} from "@/app/api";
 
 export default function VirementPage() {
+    const [userData, setUserData] = useState<{ firstname: string; name: string } | null>(null)
+    const router = useRouter();
+
+
+    useEffect(() => {
+        // Récupérez l'adresse e-mail stockée dans localStorage
+        const userMail = localStorage.getItem('userMail');
+        //console.log('userMail:', userMail);
+
+        if (userMail) {
+            fetchUserData(userMail)
+                .then((userData) => {
+                    if (userData) {
+                        setUserData(userData);
+                    }
+                })
+                .catch((error) => {
+                    console.error('Erreur lors de la récupération des données de l\'utilisateur connecté', error);
+                });
+        } else {
+            router.push('/login');
+        }
+    }, []);
+
     return (
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-lg">
             <h1 className={`${title()} text-left md:text-left`}>Plus</h1>
@@ -21,9 +47,12 @@ export default function VirementPage() {
                         <Avatar isBordered color="warning" classNames={{
                             base: "bg-gradient-to-br from-[#FFB457] to-[#FF705B]"
                         }}
-                                src="/memoji_ag.png" size="lg" />
+                                src={userData?.name === 'Guillouard' ? '/memoji_ag.png' : undefined}
+                                src={userData?.name === 'Bourre' ? '/memoji_cb.png' : undefined}
+                                name={userData?.name !== 'Guillouard' ? userData?.firstname[0] + userData?.name[0] : undefined}
+                                size="lg" />
                         <div className="mx-5 grid">
-                            <h2 className="text-lg font-semibold">Axel Guillouard</h2>
+                            <h2 className="text-lg font-semibold">{userData?.firstname} {userData?.name}</h2>
                             <p className="text-xs">Profile et données personnel</p>
                         </div>
                     </div>
