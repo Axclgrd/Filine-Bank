a83680f38a4f2eb6325639b8caa212a227a3b676
axel.gr <axel.guillouard@icloud.com>
2023-10-13 08:35:48 +0200

    modif landing

--- filine-landing/app/page.tsx
+++ filine-landing/app/page.tsx
@@ -175,14 +175,15 @@ export default function Home() {
                 <div className="flex grid-cols-12 gap-8 mt-2">
                     <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
                         <CardHeader className="absolute z-10 top-1 flex-col items-start">
-                            <p className="text-tiny text-black/60 uppercase font-bold">Nouveau</p>
-                            <h4 className="text-black font-medium text-2xl">Assurance vie</h4>
+                            <p className="text-tiny text-white/60 uppercase font-bold">Nouveau</p>
+                            <h4 className="text-white font-medium text-2xl">Assurance vie</h4>
                         </CardHeader>
                         <Image
                             removeWrapper
                             alt="Card example background"
-                            className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
-                            src="https://media.istockphoto.com/id/1199060622/fr/photo/concept-de-soins-familiaux-mains-avec-la-silhouette-de-papier-sur-la-table.jpg?s=612x612&w=0&k=20&c=1Q0CxaEU2jxkBGjXrqKduBrfEQsjFkkPTEf8JYgPQK8="
+                            className="z-0 w-full h-full -translate-y-6 object-cover"
+                            src="insurance.gif"
+                            //src="https://media.istockphoto.com/id/1199060622/fr/photo/concept-de-soins-familiaux-mains-avec-la-silhouette-de-papier-sur-la-table.jpg?s=612x612&w=0&k=20&c=1Q0CxaEU2jxkBGjXrqKduBrfEQsjFkkPTEf8JYgPQK8="
                         />
                         <CardFooter
                             className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
